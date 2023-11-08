80745ea827bcc32d163f206dfcc531d2e3a2be09
Arturo Miranda <psic.arturomiranda@gmail.com>
2023-11-08 14:28:09 -0600

    811231428

--- src/lenceria.js
+++ src/lenceria.js
@@ -1,84 +1,90 @@
 const lenceria = document.querySelector(".lenceria")
 
 class Modelo{
-    constructor(modelo,imagen,descripcion,talla,material,precio){
-    this.imagen = imagen
-    this.descripcion = descripcion
+    constructor({modelo,imagen1,imagen2,imagen3,imagen4,imagen5,imagen6,descripcion,tela,tela2,talla,precio,precio2}){
     this.modelo = modelo
+    this.imagen1 = imagen1
+    this.imagen2 = imagen2
+    this.imagen3 = imagen3
+    this.imagen4 = imagen4
+    this.imagen5 = imagen5
+    this.imagen6 = imagen6
+    this.descripcion = descripcion
+    this.tela = tela
+    this.tela2 = tela2
     this.talla = talla
-    this.material = material
     this.precio= precio
+    this.precio2= precio2
 }}
 
-let modelol1 = new Modelo(
-    "Blanco", "./media/lenceria/1.png","muy bonito","grabde","mesh","$30.oo mxn"
-)
-const modelitos = document.createElement("section")
-    modelitos.classList.add("modelo")
+let modelol1 = new Modelo({
+    modelo:"Blanco",
+    imagen1: "./media/lenceria/1.png",
+    imagen2: "",
+    imagen3: "",
+    imagen4: "",
+    imagen5: "",
+    imagen6: "",
+    descripcion: "Este modelo tiene resorte ornamentado que asegura mayor resistencia. El compuesto es de mesh y el puente de algodón para cuidar la higiene femenina en todo momento",
+    talla:"S | M | G",
+    tela:"Mesh",
+    tela2:"lycra",
+    precio:"$30.oo mxn",
+    precio2:"3 x 80.oo mxn"
+})
 
-const tituloProducto = document.createElement("h3")
-const ImgProducto = document.createElement("img")
-const descripcionProducto = document.createElement("h4")
-const pdescripcionProducto = document.createElement("p")
-const divDetalles = document.createElement("div")
-    divDetalles.classList.add("detalles")
+var productos =[modelol1]
 
-const divTalla = document.createElement("div")
-const divTela = document.createElement("div")
-const divPrecio = document.createElement("div")
 
-const h5Talla = document.createElement("h5")
-const pTalla = document.createElement("p")
-
-const h5Tela = document.createElement("h5")
-const p1Tela = document.createElement("p")
-const p2Tela = document.createElement("p")
-
-const h5Precio = document.createElement("h5")
-const p1Precio = document.createElement("p")
-const p2Precio = document.createElement("p")
-
-let tTituloProducto = document.createTextNode(`${modelol1.modelo}`)
-     ImgProducto.src = `${modelol1.imagen}`
-let tDescripcionProducto = document.createTextNode("Descripción")
-let pTdescripcionProducto = document.createTextNode(`${modelol1.descripcion}`)
-let tH5Talla = document.createTextNode("Talla")
-let tPTalla = document.createTextNode(`${modelol1.talla}`)
-let tH5Tela = document.createTextNode("Tela")
-let pp1Tela = document.createTextNode(`${modelol1.material}`)
-let tH5Precio = document.createTextNode("Precio")
-let pp1Precio = document.createTextNode(`${modelol1.precio}`)
-
-tituloProducto.appendChild(tTituloProducto)
-descripcionProducto.appendChild(tDescripcionProducto)
-pdescripcionProducto.appendChild(pTdescripcionProducto)
-h5Talla.appendChild(tH5Talla)
-pTalla.appendChild(tPTalla)
-h5Tela.appendChild(tH5Tela)
-p1Tela.appendChild(pp1Tela)
-h5Precio.appendChild(tH5Precio)
-p1Precio.appendChild(pp1Precio)
-
-lenceria.appendChild(modelitos)
-
-modelitos.appendChild(tituloProducto)
-modelitos.appendChild(ImgProducto)
-modelitos.appendChild(descripcionProducto)
-modelitos.appendChild(pdescripcionProducto)
-modelitos.appendChild(divDetalles)
-
-divDetalles.appendChild(divTalla)
-divDetalles.appendChild(divTela)
-divDetalles.appendChild(divPrecio)
-
-divTalla.appendChild(h5Talla)
-divTalla.appendChild(pTalla)
-
-divTela.appendChild(h5Tela)
-divTela.appendChild(p1Tela)
-divTela.appendChild(p2Tela)
-
-divPrecio.appendChild(h5Precio)
-divPrecio.appendChild(p1Precio)
-divPrecio.appendChild(p2Precio)
-
+productos.forEach(function(producto) {
+    var productoElement = document.createElement("div");
+    productoElement.classList.add("modelo")
+    productoElement.innerHTML = `
+      <h4>${producto.modelo}</h4>
+    <div class= "caja">
+        <div class="galeria">
+          ${producto.imagen1 ? `<img src="${producto.imagen1}" alt="">` : ''}
+          ${producto.imagen2 ? `<img src="${producto.imagen2}" alt="">` : ''}
+          ${producto.imagen3 ? `<img src="${producto.imagen3}" alt="">` : ''}
+          ${producto.imagen4 ? `<img src="${producto.imagen4}" alt="">` : ''}
+        </div>
+    </div>
+      <div class="contenedorVistaPrevia">
+        ${producto.imagen1 ? `<img src="${producto.imagen1}" alt="">` : ''}
+        ${producto.imagen2 ? `<img src="${producto.imagen2}" alt="">` : ''}
+        ${producto.imagen3 ? `<img src="${producto.imagen3}" alt="">` : ''}
+        ${producto.imagen4 ? `<img src="${producto.imagen4}" alt="">` : ''}
+      </div>
+      
+      <h4>Descripción</h4>
+      <p>${producto.descripcion}</p>
+        <div class="detalles">
+        <div>
+            <h5>Talla</h5>
+            <p>Tallas: ${producto.talla}</p>
+        </div>
+        <div>
+            <h5>Tela</h5>
+            <p>${producto.tela ? `<p>${producto.tela}</p>`:''}
+            <p>${producto.tela2 ? `<p>${producto.tela2}</p>`:''}
+        </div>
+        <div>
+            <h5>Precio</h5>
+            <p>${producto.precio ? `<p>${producto.precio}</p>`:''}
+            <p>${producto.precio2 ? `<p>${producto.precio2}</p>`:''}
+        </div>    
+    </div>`;
+  
+    // Calcula el ancho del contenedor .galeria en función de la cantidad de imágenes
+    var numImagenes = 0;
+    if (producto.imagen) numImagenes++;
+    if (producto.imagen2) numImagenes++;
+    if (producto.imagen3) numImagenes++;
+    if (producto.imagen4) numImagenes++;
+  
+    // Aplica el ancho calculado al contenedor .galeria
+    var galeriaElement = productoElement.querySelector(".galeria");
+    galeriaElement.style.width = numImagenes * 100 + "%";
+  
+    lenceria.appendChild(productoElement);
+  });
