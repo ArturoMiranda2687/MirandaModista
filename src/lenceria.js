c18baa1f0b236615bcc3959831af9404aff91e2c
Arturo Miranda <psic.arturomiranda@gmail.com>
2023-11-09 18:13:11 -0600

    0911231813

--- src/lenceria.js
+++ src/lenceria.js
@@ -49,35 +49,34 @@ productos.forEach(function(producto) {
           ${producto.imagen4 ? `<img src="${producto.imagen4}" alt="">` : ''}
         </div>
     </div>
-      <div class="contenedorVistaPrevia">
-        ${producto.imagen1 ? `<img src="${producto.imagen1}" alt="">` : ''}
-        ${producto.imagen2 ? `<img src="${producto.imagen2}" alt="">` : ''}
-        ${producto.imagen3 ? `<img src="${producto.imagen3}" alt="">` : ''}
-        ${producto.imagen4 ? `<img src="${producto.imagen4}" alt="">` : ''}
+    <div class="contenedorVistaPrevia">
+      ${producto.imagen1 ? `<img src="${producto.imagen1}" alt="">` : ''}
+      ${producto.imagen2 ? `<img src="${producto.imagen2}" alt="">` : ''}
+      ${producto.imagen3 ? `<img src="${producto.imagen3}" alt="">` : ''}
+      ${producto.imagen4 ? `<img src="${producto.imagen4}" alt="">` : ''}
+    </div>
+    <h4>Descripción</h4>
+    <p>${producto.descripcion}</p>
+    <div class="detalles">
+      <div>
+          <h5>Talla</h5>
+          <p>Tallas: ${producto.talla}</p>
       </div>
-      
-      <h4>Descripción</h4>
-      <p>${producto.descripcion}</p>
-        <div class="detalles">
-        <div>
-            <h5>Talla</h5>
-            <p>Tallas: ${producto.talla}</p>
-        </div>
-        <div>
-            <h5>Tela</h5>
-            <p>${producto.tela ? `<p>${producto.tela}</p>`:''}
-            <p>${producto.tela2 ? `<p>${producto.tela2}</p>`:''}
-        </div>
-        <div>
-            <h5>Precio</h5>
-            <p>${producto.precio ? `<p>${producto.precio}</p>`:''}
-            <p>${producto.precio2 ? `<p>${producto.precio2}</p>`:''}
-        </div>    
+      <div>
+          <h5>Tela</h5>
+          <p>${producto.tela ? `<p>${producto.tela}</p>`:''}
+          <p>${producto.tela2 ? `<p>${producto.tela2}</p>`:''}
+      </div>
+      <div>
+          <h5>Precio</h5>
+          <p>${producto.precio ? `<p>${producto.precio}</p>`:''}
+          <p>${producto.precio2 ? `<p>${producto.precio2}</p>`:''}
+      </div>    
     </div>`;
   
     // Calcula el ancho del contenedor .galeria en función de la cantidad de imágenes
     var numImagenes = 0;
-    if (producto.imagen) numImagenes++;
+    if (producto.imagen1) numImagenes++;
     if (producto.imagen2) numImagenes++;
     if (producto.imagen3) numImagenes++;
     if (producto.imagen4) numImagenes++;
